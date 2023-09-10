ae3bcba875325510b22c62fae582a88819dddcdc
Weizhi Peng <36948666+Miraclove@users.noreply.github.com>
2023-09-10 17:26:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,10 +17,67 @@ sed -i 's/192.168.100.1/192.168.6.1/g' package/base-files/files/bin/config_gener
 sed -i 's/OpenWrt/NetGate/g' package/base-files/files/bin/config_generate
 
 # ttyd 自动登录
-sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
+# sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
 
 # 添加自定义软件包
-echo '
-CONFIG_PACKAGE_luci-app-adguardhome=y
-CONFIG_PACKAGE_luci-app-passwall=y
-' >> .config
+ echo '
+ CONFIG_PACKAGE_luci-app-poweroff=y 
+ CONFIG_PACKAGE_luci-i18n-poweroff-zh-cn=y 
+ CONFIG_PACKAGE_luci-app-openclash=y 
+ CONFIG_PACKAGE_kmod-inet-diag=y 
+ CONFIG_PACKAGE_luci-app-aliyundrive-webdav=y 
+ CONFIG_PACKAGE_luci-app-passwall2=y 
+ CONFIG_PACKAGE_luci-app-passwall=y 
+ CONFIG_PACKAGE_luci-app-passwall_Transparent_Proxy=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ChinaDNS_NG=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Haproxy=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Hysteria=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_IPv6_Nat=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Kcptun=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_NaiveProxy=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Simple_Obfs=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_GO=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Plugin=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray=y 
+ CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray_Plugin=y 
+ CONFIG_PACKAGE_luci-app-ssr-plus=y 
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NONE_V2RAY=y 
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDEINCLUDE_V2ray=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDEINCLUDE_Xray=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDEINCLUDE_Kcptun=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDEINCLUDE_Hysteria=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDEINCLUDE_IPT2Socks=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDEINCLUDE_NaiveProxy=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDEINCLUDE_Redsocks2=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Client=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_Libev_Client=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_Rust_Client=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Server=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_Libev_Server=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_Rust_Server=y  
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_Simple_Obfs=y 
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_V2ray_Plugin=y 
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_ShadowsocksR_Libev_Client=y 
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_ShadowsocksR_Libev_Server=y 
+ CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Trojan=y 
+ CONFIG_PACKAGE_luci-app-vssr=y 
+ CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Xray=y 
+ CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Trojan=y 
+ CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Kcptun=y 
+ CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Xray_plugin=y 
+ CONFIG_PACKAGE_luci-app-vssr_INCLUDE_ShadowsocksR_Server=y 
+ CONFIG_PACKAGE_luci-app-bypass=y 
+ CONFIG_PACKAGE_luci-app-turboacc=y 
+ CONFIG_PACKAGE_luci-app-adguardhome=y 
+ CONFIG_PACKAGE_luci-app-mosdns=y 
+ CONFIG_PACKAGE_luci-app-smartdns=y 
+ ' >> .config
