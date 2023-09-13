5cfcef511947ebf9131547081cb8610aea431736
Weizhi Peng <36948666+Miraclove@users.noreply.github.com>
2023-09-13 11:47:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 echo 'src-git stone https://github.com/s71557/IstoreOS-Packages.git;master' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git;master' >>feeds.conf.default
